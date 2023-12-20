89f3c377c32c9ebab3aea0308afc541e8b5f3fe8
b-ma <benjamin.matuszewski@gmail.com>
2023-12-20 16:08:04 +0100

    feat: sync plugin tutorial

---
+++ plugin-sync/src/server/index.js
@@ -0,0 +1,39 @@
+import '@soundworks/helpers/polyfills.js';
+import { Server } from '@soundworks/core/server.js';
+import pluginSync from '@soundworks/plugin-sync/server.js';
+import pluginPlatformInit from '@soundworks/plugin-platform-init/server.js';
+
+import { loadConfig } from '../utils/load-config.js';
+import '../utils/catch-unhandled-errors.js';
+
+// - General documentation: https://soundworks.dev/
+// - API documentation:     https://soundworks.dev/api
+// - Issue Tracker:         https://github.com/collective-soundworks/soundworks/issues
+// - Wizard & Tools:        `npx soundworks`
+
+const config = loadConfig(process.env.ENV, import.meta.url);
+
+console.log(`
+--------------------------------------------------------
+- launching "${config.app.name}" in "${process.env.ENV || 'default'}" environment
+- [pid: ${process.pid}]
+--------------------------------------------------------
+`);
+
+/**
+ * Create the soundworks server
+ */
+const server = new Server(config);
+// configure the server for usage within this application template
+server.useDefaultApplicationTemplate();
+// register plugins
+server.pluginManager.register('platform-init', pluginPlatformInit);
+server.pluginManager.register('sync', pluginSync);
+
+/**
+ * Launch application (init plugins, http server, etc.)
+ */
+await server.start();
+
+// and do your own stuff!
+
